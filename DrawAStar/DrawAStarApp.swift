ab903d7d481ec7be9408886a0d4286d1d65c581b
id2k1149 <id2k1149@gmail.com>
2023-05-10 12:23:19 -0400

    Initial Commit

---
+++ DrawAStar/DrawAStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawAStarApp.swift
+//  DrawAStar
+//
+//  Created by Max Franz Immelmann on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawAStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
